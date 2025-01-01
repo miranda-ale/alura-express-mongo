5c45b5239d1d92a72b29ca28b74ed776d007764b
miranda-ale <alessandro.siqueira@outlook.com>
2025-01-01 20:29:54 -0300

    criacao das rotas e configuracao do nodemon

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from 'http';
 
 const PORT = 3000;
 
+const rotas = {
+    '/': 'Curso de Express API',
+    '/livros': 'Rota livros',
+    '/autores': 'Rota autores'
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type' : 'text/plain'});
-    res.end('Curso de Node.js');
+    res.end(rotas[req.url]);
 });
 
 
