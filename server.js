8b43149dc66d42739910fb81e9f012e0aeb2ecc8
miranda-ale <alessandro.siqueira@outlook.com>
2025-01-01 20:02:34 -0300

    montagem do servidor

---
+++ server.js
@@ -0,0 +1,15 @@
+import http from 'http';
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type' : 'text/plain'});
+    res.end('Curso de Node.js');
+});
+
+
+server.listen(PORT, () => {
+    console.log(`Servidor escutando na Porta ${PORT} \nhttp://localhost:${PORT}`)
+});
+
+
