94e98067b7f2611391216ba70647b48965de30e7
miranda-ale <alessandro.siqueira@outlook.com>
2025-01-01 21:49:39 -0300

    atualizacao da lista de livros

--- src/app.js
+++ src/app.js
@@ -1,32 +1,68 @@
 import express from "express";
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 const livros = [
-  {
-    id: 1,
-    titulo: "O Senhor dos Anéis",
-  },
-  {
-    id: 2,
-    titulo: "O Hobbit",
-  },
-{
-    id: 3,
-    titulo: "Silmarillion",
-},
-{
-    id: 4,
-    titulo: "Contos Inacabados",
-}
+    {
+        id: 1,
+        titulo: "O Senhor dos Anéis: A Sociedade do Anel",
+    },
+    {
+        id: 2,
+        titulo: "O Senhor dos Anéis: As Duas Torres",
+    },
+    {
+        id: 3,
+        titulo: "O Senhor dos Anéis: O Retorno do Rei",
+    },
+    {
+        id: 4,
+        titulo: "O Hobbit",
+    },
+    {
+        id: 5,
+        titulo: "Silmarillion",
+    },
+    {
+        id: 6,
+        titulo: "Contos Inacabados",
+    },
+    {
+        id: 7,
+        titulo: "Os Filhos de Húrin",
+    },
+    {
+        id: 8,
+        titulo: "Beren e Lúthien",
+    },
+    {
+        id: 9,
+        titulo: "A Queda de Gondolin",
+    },
+    {
+        id: 10,
+        titulo: "A História de Kullervo",
+    },
+    {
+        id: 11,
+        titulo: "A Lenda de Sigurd e Gudrún",
+    },
+    {
+        id: 12,
+        titulo: "A Balada de Aotrou e Itroun",
+    },
+    {
+        id: 13,
+        titulo: "A Última Canção de Bilbo",
+    },
 ];
 
 // Localiza um livro pela ID
 function buscaLivro(id) {
-    return livros.findIndex(livro => {
-        return livro.id === Number(id)
-});
+  return livros.findIndex((livro) => {
+    return livro.id === Number(id);
+  });
 }
 
 app.get("/", (req, res) => {
@@ -39,29 +75,29 @@ app.get("/livros", (req, res) => {
 });
 
 // Busca livro por ID
-app.get("/livros/:id" , (req, res) => {
-    const index = buscaLivro(req.params.id);
-    res.status(200).json(livros[index]);
+app.get("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  res.status(200).json(livros[index]);
 });
 
 // Cria novo livro
 app.post("/livros", (req, res) => {
   livros.push(req.body);
-  res.status(201).send('Livro criado com sucesso.');
+  res.status(201).send("Livro criado com sucesso.");
 });
 
 // Atualiza livro
 app.put("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.status(200).send('Livro atualizado com sucesso');
-})
+  const index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.status(200).json(livros);
+});
 
 // Exclui um livro
 app.delete("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros.splice(index, 1);
-    res.status(200).send('Livro excluído com sucesso.');
+  const index = buscaLivro(req.params.id);
+  livros.splice(index, 1);
+  res.status(200).send("Livro excluído com sucesso.");
 });
 
 export default app;
