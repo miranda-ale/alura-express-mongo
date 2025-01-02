7aebf3eebeff7551d37f56d5672329d689297390
miranda-ale <alessandro.siqueira@outlook.com>
2025-01-01 21:29:17 -0300

    criacao de rotas para criacao, busca e exclusao de livro

--- src/app.js
+++ src/app.js
@@ -1,25 +1,52 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
+app.use(express.json())
 
 const livros = [
-    {
-        id: 1,
-        titulo: 'O Senhor dos Anéis'
-    },
-    {
-        id: 2,
-        titulo: 'O Hobbit'
-    }
-]
+  {
+    id: 1,
+    titulo: "O Senhor dos Anéis",
+  },
+  {
+    id: 2,
+    titulo: "O Hobbit",
+  },
+];
 
-app.get('/', (req, res) => {
-    res.status(200).send('Curso de Node.js')
+function buscaLivro(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id)
+});
+}
+
+app.get("/", (req, res) => {
+  res.status(200).send("Curso de Node.js");
 });
 
-app.get('/livros', (req, res) => {
-    res.status(200).json(livros);
+// Lista geral de livros
+app.get("/livros", (req, res) => {
+  res.status(200).json(livros);
 });
 
+// Busca livro por ID
+app.get("/livros/:id" , (req, res) => {
+    const index = buscaLivro(req.params.id);
+    res.status(200).json(livros[index]);
+});
+
+// Cria novo livro
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+  res.status(201).send('Livro criado com sucesso.')
+});
+
+
+// Exclui um livro
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send('Livro excluído com sucesso.')
+});
 
 export default app;
