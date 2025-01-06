8cb84b7237873bcc9733ef862410337791b34794
miranda-ale <alessandro.siqueira@outlook.com>
2025-01-06 08:06:16 -0300

    tratamento de erro de validação

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -1,13 +1,20 @@
 import mongoose from "mongoose";
+import ErroBase from "../error/ErroBase.js";
 
 function manipuladorDeErros(error, req, res, next) {
+	console.error("Erro: " + error.name + " - " + error.message);
+	
 	if (error instanceof mongoose.Error.CastError) {
-		res.status(400).send({ message: "Dados fornecidos incorretos" });
+		res.status(400).send({ message: "Dados fornecidos incorretos!" });
+	} else if(error instanceof mongoose.Error.ValidationError) {
+		const mensagensErro = Object.values(error.errors)
+		.map(error => error.message)
+		.join("; ");
+
+		res.status(400).send({ message: `Erro de validação de dados: ${mensagensErro}` });
 	} else {
-		res
-			.status(500)
-			.send({ message: `${error.message} - Erro interno do servidor!` });
-	}
+		new ErroBase().enviarResposta(res);
+	};
 };
 
 export default manipuladorDeErros;
--- src/models/livroModel.js
+++ src/models/livroModel.js
@@ -4,7 +4,9 @@ import { autorSchema } from "./AutorModel.js";
 // Interface de relacionamento com o Atlas
 const livroSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
-    titulo: { type: String, required: true },
+    titulo: { 
+        type: String, 
+        required: [true, "O título do livro é obrigatório!"] },
     editora: { type: String },
     preco: { type: Number },
     paginas: { type: Number },
