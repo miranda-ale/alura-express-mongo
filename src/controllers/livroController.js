e33f60abf14c0531c754052460943ab9ee6cbee7
miranda-ale <alessandro.siqueira@outlook.com>
2025-01-07 08:22:48 -0300

    refatoracao da controladora livro, validacao de campos do modelo livro

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,5 +1,5 @@
-import { autorModel } from "../models/AutorModel.js";
 import livroModel from "../models/livroModel.js";
+import NaoEncontrado from "../error/NaoEncontrado.js";
 
 class LivroController {
 	static async listarLivros(req, res, next) {
@@ -14,73 +14,64 @@ class LivroController {
 	static async listarLivroPorId(req, res, next) {
 		try {
 			const resultadoLivro = await livroModel.findById(req.params.id);
-			if(resultadoLivro !== null) {
+			if (resultadoLivro !== null) {
 				res.status(200).send({
-				  message: 'Livro encontrado com sucesso!',
+					message: "Livro encontrado com sucesso!",
+					livro: resultadoLivro
 				});
-			  } else {
+			} else {
 				next(new NaoEncontrado("Livro não localizado!"));
-			  };
-		
+			}
 		} catch (error) {
 			next(error);
-		}
-	}
+		};
+	};
 
 	static async criarLivro(req, res, next) {
-		const novoLivro = req.body;
-
 		try {
-			const autorEncontrado = await autorModel.findById(novoLivro.autor);
-			if (!autorEncontrado) {
-				res.status(404).send({
-					message: "Autor não encontrado",
-				});
-			}
-
-			const livroCompleto = {
-				...novoLivro,
-				autor: { ...autorEncontrado._doc },
-			};
-			const livroCriado = await livroModel.create(livroCompleto);
+			const novoLivro = new livroModel(req.body);
+			const livroCriado = await novoLivro.save();
 			res.status(201).send({
 				message: "Livro criado com sucesso!",
 				livro: livroCriado,
 			});
 		} catch (error) {
 			next(error);
-		}
-	}
+		};
+	};
 
 	static async atualizarLivro(req, res, next) {
 		try {
-			const resultadoLivro = await livroModel.findByIdAndUpdate(req.params.id, req.body);
-			if(resultadoLivro !== null) {
+			const resultadoLivro = await livroModel.findByIdAndUpdate(
+				req.params.id,
+				req.body,
+			);
+			if (resultadoLivro !== null) {
 				res.status(200).send({
-				  message: 'Livro encontrado com sucesso!',
+					message: "Livro atualizado com sucesso!",
 				});
-			  } else {
+			} else {
 				next(new NaoEncontrado("Livro não localizado!"));
-			  };
+			}
 		} catch (error) {
 			next(error);
-		}
-	}
+		};
+	};
 
 	static async excluirLivro(req, res, next) {
 		try {
 			const resultadoLivro = await livroModel.findByIdAndDelete(req.params.id);
-			if(resultadoLivro !== null) {
+			if (resultadoLivro !== null) {
 				res.status(200).send({
-				  message: 'Livro encontrado com sucesso!',
+					message: "Livro excluído com sucesso!",
 				});
-			  } else {
+			} else {
 				next(new NaoEncontrado("Livro não localizado!"));
-			  };
+			}
 		} catch (error) {
 			next(error);
-		}
-	}
+		};
+	};
 
 	static async listarLivrosPorEditora(req, res, next) {
 		const editora = req.query.editora;
@@ -92,8 +83,8 @@ class LivroController {
 			});
 		} catch (error) {
 			next(error);
-		}
-	}
-}
+		};
+	};
+};
 
 export default LivroController;
--- src/models/livroModel.js
+++ src/models/livroModel.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import { autorSchema } from "./AutorModel.js";
 
 // Interface de relacionamento com o Atlas
 const livroSchema = new mongoose.Schema({
@@ -7,10 +6,28 @@ const livroSchema = new mongoose.Schema({
     titulo: { 
         type: String, 
         required: [true, "O título do livro é obrigatório!"] },
-    editora: { type: String },
     preco: { type: Number },
-    paginas: { type: Number },
-    autor: autorSchema
+    paginas: { 
+        type: Number,
+        validate: {
+            validator: (value) => {
+            return value >= 10 && value <= 5000;},
+            message: "Número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"
+        }
+     },
+     autor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "autores",
+        required: [true, "O(a) autor(a) é obrigatório"]
+      },
+      editora: {
+        type: String,
+        required: [true, "A editora é obrigatória"],
+        enum: {
+            values: ["Clássicos", "Modernos", "Romance", "Ficção"],
+            message: "A editora {VALUE} não é um valor permitido!"
+        }
+      }
 }, { versionKey: false });
 
 const livroModel = mongoose.model("livros", livroSchema);
