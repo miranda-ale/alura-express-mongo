de5503b3518f17a3d9c94f8796a00491d73ce9f0
miranda-ale <alessandro.siqueira@outlook.com>
2025-01-07 07:12:16 -0300

    tratamento de erro 404

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import express from "express";
 import conectaDB from "./config/dbConnect.js";
 import routes from "./routes/appRoutes.js";
+import manipulador404 from "./middlewares/manipulador404.js";
 import manipuladorDeErros from "./middlewares/manipuladorDeErros.js";
 
 await conectaDB();
 const app = express();
 routes(app);
+
+app.use(manipulador404);
+
 app.use(manipuladorDeErros);
 
 export default app;
--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import ErroBase from "../error/ErroBase.js";
 import RequisicaoIncorreta from "../error/RequisicaoIncorreta.js";
 import ErroValidacao from "../error/ErroValidacao.js";
+import NaoEncontrado from "../error/NaoEncontrado.js";
 
 function manipuladorDeErros(error, req, res, next) {
 	console.error("Erro: " + error.name + " - " + error.message);
@@ -10,6 +11,8 @@ function manipuladorDeErros(error, req, res, next) {
 		new RequisicaoIncorreta().enviarResposta(res);
 	} else if(error instanceof mongoose.Error.ValidationError) {
 		new ErroValidacao(error).enviarResposta(res);
+	} else if(error instanceof NaoEncontrado) {
+		error.enviarResposta(res);
 	} else {
 		new ErroBase().enviarResposta(res);
 	};
